c748c79ec07cf727937d442289a10313eea736c0
demoraes <gabrielmoraes_2013@outlook.com>
2020-06-29 21:22:08 -0400

    environment variable

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '035ae1af4e7917',
-    pass: '950e93474ad495',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
