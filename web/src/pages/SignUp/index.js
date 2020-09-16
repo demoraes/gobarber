cfd34a1efdabc94bb3e3a5d181546fc02dc2729f
demoraes <gabrielmoraes_2013@outlook.com>
2020-09-15 21:44:13 -0400

    pages sigIn and sigUp styled

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-function SignUp() {
-  return <h1>SignUp</h1>;
+export default function SignUp() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Sua e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
-
-export default SignUp;
