a11c53ff62fc8d075bc064a677fd29ce79dbb3a5
demoraes <gabrielmoraes_2013@outlook.com>
2020-10-06 23:21:59 -0400

    page profile styled and configurati routes change

---
+++ mobile/src/pages/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+function Profile() {
+  return <Background />;
+}
+
+export default Profile;
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -9,6 +10,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 const AppStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -19,8 +21,37 @@ export default function Routes() {
   return (
     <NavigationContainer>
       {isSignedIn === true ? (
-        <Tab.Navigator>
-          <Tab.Screen name="Dashboard" component={Dashboard} />
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintColor: '#fff',
+            inactiveTintColor: 'rgba(255,255,255,0.6)',
+            style: {
+              backgroundColor: '#8d41a8',
+            },
+          }}
+        >
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              tabBarLabel: 'Meu Perfil',
+
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="person" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Dashboard"
+            component={Dashboard}
+            options={{
+              tabBarLabel: 'Agendamentos',
+
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="event" size={size} color={color} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       ) : (
         <AppStack.Navigator headerMode="none">
